e76b080a6e5809d08be5d464b87221c54e58fd98
Samuel Gernstetter <sgern@users.noreply.github.com>
2015-01-12 18:20:16 -0500

    Revert "hjfhjgfhj"
    This reverts commit 3b2321038f345e38a948cdd3e305c2ab4440b562.

--- BadRobot2015/src/org/usfirst/frc/team1014/robot/OI.java
+++ BadRobot2015/src/org/usfirst/frc/team1014/robot/OI.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  * 
- * @author Steve Popovichvvv
+ * @author Steve Popovich
  */
 public class OI {
 
